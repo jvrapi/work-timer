c0e758af41c3cff9669691205961dc07b71f4f28
jvrapi <joaooviitorr@hotmail.com>
2022-09-14 07:08:07 -0300

    updated styles and data

--- client/src/pages/schedules-register/index.tsx
+++ client/src/pages/schedules-register/index.tsx
@@ -6,10 +6,34 @@ import { Button } from '../../components/Button';
 export function SchedulesRegister() {
   const [timerInitiated, setTimerInitiated] = useState(false);
 
+  const data = [
+    {
+      date: '12/09/2022',
+      started_at: '08:00',
+      finished_at: '18:00',
+      started_lunch_at: '12:00',
+      finished_lunch_at: '13:00',
+      worked_time: '8 horas',
+    },
+    {
+      date: '13/09/2022',
+      started_at: '08:00',
+      finished_at: '18:00',
+      started_lunch_at: '12:00',
+      finished_lunch_at: '13:00',
+      worked_time: '8 horas',
+    },
+  ];
+
   function startLaunchTime() {}
 
+  function buildTime(started_at: string, finished_at: string) {
+    const time = `${started_at} - ${finished_at}`;
+    return time;
+  }
+
   return (
-    <div className="flex justify-center h-full w-full text-center flex-col bg-secondary p-1">
+    <div className="flex justify-center h-full w-full text-center flex-col bg-secondary p-6">
       <div className="self-end flex">
         <div className="mr-2">
           <Button
@@ -36,29 +60,21 @@ export function SchedulesRegister() {
       <table className="table-auto w-full bg-gray-900 rounded-lg mt-10 text-textColor">
         <thead>
           <tr>
-            <th>data</th>
-            <th>horário</th>
-            <th>almoço</th>
-            <th>horas trabalhadas</th>
+            <th>Data</th>
+            <th>Horário</th>
+            <th>Almoço</th>
+            <th>Horas trabalhadas</th>
           </tr>
         </thead>
         <tbody>
-          <tr
-            className="hover:bg-secondaryHover"
-            onClick={() => console.error('deu erro hein')}
-          >
-            <td>13/09/2022</td>
-            <td>08:00 - 18:00</td>
-            <td>13:00 - 14:00</td>
-            <td>8 Horas</td>
-          </tr>
-
-          <tr>
-            <td>13/09/2022</td>
-            <td>08:00 - 18:00</td>
-            <td>13:00 - 14:00</td>
-            <td>8 Horas</td>
-          </tr>
+          {data.map((schedule, index) => (
+            <tr key={index} className="hover:bg-secondaryHover">
+              <td>{schedule.date}</td>
+              <td>{buildTime(schedule.started_at, schedule.finished_at)}</td>
+              <td>{buildTime(schedule.started_lunch_at, schedule.finished_lunch_at)}</td>
+              <td>{schedule.worked_time}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
